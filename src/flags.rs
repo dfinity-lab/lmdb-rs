a244a247789fd9ece0fe1a406180c32e5bbc0c3f
Nicolas Mattia <nicolas@nmattia.com>
2026-07-23 13:40:25 +0200

    Bump bitflags to 2

--- src/flags.rs
+++ src/flags.rs
@@ -4,7 +4,7 @@ use ffi::*;
 
 bitflags! {
     #[doc="Environment options."]
-    #[derive(Default)]
+    #[derive(Default, Clone, Copy, Debug, PartialEq, Eq)]
     pub struct EnvironmentFlags: c_uint {
 
         #[doc="Use a fixed address for the mmap region. This flag must be specified"]
